ee1e669dcdab453aeba6675f00469380b5d473ee
Simarjit Singh <simar.rajput@gmail.com>
2019-04-13 12:35:27 -0700

    Checked in for Project, Owner
    Commited code for default page. Made projects and owner calls.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var express = require("express");
 var axios = require("axios");
+const open = require('open');
 
 const PORT = 3000;
 const app = express();
@@ -9,37 +10,89 @@ const apiKey = 'R1KsBKXw3uuglqr3';
 const clientId = 'azFF3TJCVUB52CHYw8GssO3htAbWW2ze3u8ZKkblI1zZdv7u';
 const clientSecret = '4bAXqGTS78tJ6srYJiKIdsMiH2vawXG1ATJRDGriQQ9O1d5H';
 
-var accessCode = '';
 var accessToken = '';
+var appResponse = '';
 var callBackUrl = "/oauthCallback";
 var accessTokenUrl = 'https://hackaday.io/access_token';
 var projectsUrl = 'http://api.hackaday.io/v1/projects';
+var usersUrl = 'http://api.hackaday.io/v1/users/batch';
+var singleUserUrl = 'http://api.hackaday.io/v1/users/';
 
 app.set('view engine', 'ejs');
 
-app.get(callBackUrl, (req, res) => {
-  console.log("Entered Callback");
-  accessCode = req.query.code;
-  console.log('Request Code: ' + req.query.code);
-  axios.get(accessTokenUrl, { params: { client_id: clientId, client_secret: clientSecret, code: accessCode, grant_type: 'authorization_code' } })
-    .then((response) => {
-      accessToken = response.data.access_token;
-      getProducts(res);
-    })
-    .catch((error) => {
-        console.log('Error occured: ' + error);
-    })
+app.get('/GetUserData', (request, response) => {
+  var ownerId = request.query.ownerId;
+  axios.get(singleUserUrl + ownerId, { params: { api_key: apiKey }, headers: { 'Authorization': 'token ' + accessToken } })
+  .then((result) => {
+    response.send(result.data);
+  });
 });
 
-function getProducts(res){
-  axios.get(projectsUrl, { params: { api_key: apiKey }, headers: { 'Authorization': 'token ' + accessToken } })
-    .then((response) => {
-      console.log(response.data.projects);
-      res.render('pages/index', {projects: response.data.projects, title: 'Supplyframe Projects'});
-    })
-    .catch((error) => {
-        console.log('Error occured: ' + error);
-    })
+app.get('/', () => {
+  open('https://hackaday.io/authorize?response_type=code&client_id=' + clientId);
+});
+
+app.get(callBackUrl, (request, response) => {
+  appResponse = response;
+  var accessCode = request.query.code;
+  axios.get(accessTokenUrl, { params: { client_id: clientId, client_secret: clientSecret, code: accessCode, grant_type: 'authorization_code' } })
+  .then((result) => {
+    accessToken = result.data.access_token;
+    GetProducts();
+  })
+  .catch((error) => {
+    appResponse.render('pages/error', { error: error , title: 'Error Occured' });
+  });
+});
+
+function GetProducts(){
+  return axios.get(projectsUrl, { params: { api_key: apiKey }, headers: { 'Authorization': 'token ' + accessToken } })
+  .then((result) => {
+    GetUsers(result.data.projects);
+  }).catch((error) => {
+    appResponse.render('pages/error', { error: error , title: 'Error Occured' });
+  });
+}
+
+function GetUsers(projects) {
+  var userIds  = [];
+  
+  for(var key in projects)
+    userIds.push(projects[key].owner_id);
+
+  return axios.get(usersUrl, { params: { api_key: apiKey, ids: userIds.join(',') }, headers: { 'Authorization': 'token ' + accessToken } })
+  .then((result) => {
+    var pageData = BuildPageData(projects, result.data.users);
+    appResponse.render('pages/index', { pageData: pageData, title: 'Supplyframe Projects' });
+  })
+  .catch((error) => {
+    appResponse.render('pages/error', { error: error , title: 'Error Occured' });
+  });
+}
+
+function BuildPageData(projects, users){
+  var pageData = [];
+  
+  for(var key in projects){
+    var pageRow = {};
+    
+    pageRow['name'] = projects[key].name;
+    pageRow['summary'] = projects[key].summary;
+    pageRow['image_url'] = projects[key].image_url;
+    pageRow['url'] = projects[key].url;
+    pageRow['owner_name'] = GetUserInfo(projects[key].owner_id, users).screen_name;
+    pageRow['owner_id'] = projects[key].owner_id;
+
+    pageData.push(pageRow);
+  }
+  return pageData;
+}
+
+function GetUserInfo(userId, users){
+  for(var key in users){
+    if(users[key].id == userId)
+      return users[key];
+  }
 }
 
 app.listen(PORT);
